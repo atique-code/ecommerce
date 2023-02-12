717a7916312ad8d79d4b070afe434cfff483a48f
Atique <atiquekalwar123@gmail.com>
2023-02-12 14:44:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var mainImg = document.getElementById("mainImg");
+// var smallImg = document.getElementsByClassName("small-img");
+
+// smallImg[0].onclick = function() {
+//     mainImg.src = smallImg[0].src
+// }
+
+// smallImg[1].onclick = function() {
+//     mainImg.src = smallImg[1].src
+// }
+
+// smallImg[2].onclick = function() {
+//     mainImg.src = smallImg[2].src
+// }
+
+// smallImg[3].onclick = function() {
+//     mainImg.src = smallImg[3].src
+// }
+
+function chnageimg(e){
+  console.log(e)
+  mainImg.src=e.src
+}
